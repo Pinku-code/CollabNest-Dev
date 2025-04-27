f28fc2a52c57b27c06d36f8acdf6f45b48d04f22
Manish3air <manishpandey3365@gmail.com>
2025-04-28 04:29:50 +0530

    update

--- Backend/server.js
+++ Backend/server.js
@@ -26,7 +26,7 @@ app.use(express.json());
 // ✅ CORS config
 app.use(
   cors({
-    origin: ["http://localhost:5173", "https://job-tracker-tx9v.onrender.com"],
+    origin: ["http://localhost:5173", "https://collab-nest-dev.vercel.app"],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE"],
   })
